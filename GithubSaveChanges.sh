666feaf731363aa5219f006172fab6d28ce69112
Jesus Castellanos Paez <jesus.castellanos@solem.cl>
2020-09-19 11:43:05 -0500

    messageCommit
    Signed-off-by: Jesus Castellanos Paez <jesus.castellanos@solem.cl>

--- GithubSaveChanges.sh
+++ GithubSaveChanges.sh
@@ -1,5 +1,20 @@
-messageCommit=${1};
-origin=${2};
-echo "${messageCommit}" "${origin}";
-git add .
-git commit -asm messageCommit;
+repoName=${1};
+messageCommit=${2};
+tagName=${3};
+
+if [ repoName!="" -a messageCommit!="" -a tagName!="" ]
+    then
+        ruta='/Documents/github/';
+            cd  ~/$ruta/$repoName;
+        echo "En la ruta `pwd`";
+        git add .
+        git commit -asm messageCommit;
+        git tag -a ${tagName} -m ${messageCommit};
+        git push origin;
+        git push --tags;
+else
+    echo "Ingresar:";
+    echo "repoName";
+    echo "messageCommit";
+    echo "tagName";
+fi
